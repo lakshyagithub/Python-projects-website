0e50c231818bac0e9d4a4d52e2d9a03a87a9a54a
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2023-01-01 22:11:31 +0530

    Some update

--- colors.py
+++ colors.py
@@ -6,3 +6,14 @@ Created on Sat Dec 31 20:58:28 2022
 """
 
 from tkinter import *
+import random
+
+root = Tk()
+root.geometry("400x200")
+root.title("Colors")
+root.configure(background="White")
+
+label1 = Label(root, bg="white", font=(15), fg="green")
+label1.pack()
+
+root.mainloop()
