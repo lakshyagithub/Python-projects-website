865dca3149dbf0d9d88f36e52e384ac5b3270c23
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2023-01-09 15:16:36 +0530

    Add files via upload

--- colors.py
+++ colors.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Sat Dec 31 20:58:28 2022
@@ -13,8 +14,11 @@ root.geometry("400x200")
 root.title("Colors")
 root.configure(background="sky blue")
 
-label1 = Label(root, bg="sky blue", font=(40), fg="green")
+label1 = Label(root, bg="sky blue", font=("Arial", 40), fg="green")
 label1.pack()
+
+text_box = Entry()
+text_box.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 class game:
     def __init__(self):
@@ -27,10 +31,17 @@ class game:
         label1["fg"] = self.color[self.random_color]
         print(self.text[self.random_color])
         print(self.color[self.random_color])
+    def checkAnswer(self):
+        self.random_text = self.text[self.random_color]
+        print(self.random_text)
+        if text_box.get() == self.random_text:
+            print("Correct!")
 
 obj1 = game()
 
-btn1 = Button(root, text="Start", command=obj1.updateGame, font=(15), bg="light green", fg="white", bd=0)
+btn1 = Button(root, text="Start/Update color", command=obj1.updateGame, font=(15), bg="light green", fg="white", bd=0)
 btn1.place(relx=0.7, rely=0.7, anchor=CENTER)
+btn2 = Button(root, text="Check", command=obj1.checkAnswer, font=(15), bg="light green", fg="white", bd=0)
+btn2.place(relx=0.2857142857142857, rely=0.7, anchor=CENTER)
 
 root.mainloop()
